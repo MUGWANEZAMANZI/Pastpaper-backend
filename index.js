b92af2d8a3c231b11fc4f6999cc28c79858a5683
MUGWANEZA MANZI Audace <mmaudace@gmail.com>
2026-04-28 11:58:09 +0200

    DOM and eventListineing implemented

---
+++ index.js
@@ -0,0 +1,63 @@
+const guestPage= `
+<div>
+<form id="login-form">
+<label>Email:</label>
+<input type="email" name="email" required>
+<br>
+<label>Password:</label>
+<input type="password" name="password" required>
+<br>
+<button type="submit">Login</button>
+</form>
+</div>      
+`;
+
+const memberPage = `
+<div>You are logged in</div>
+<button id="logout">Logout</button>
+`;
+
+let isLoggedIn = false;
+const page = isLoggedIn ? memberPage : guestPage;
+
+document.addEventListener('DOMContentLoaded', function() {
+  const mainElement = document.getElementById('main');
+  if (mainElement) {
+    mainElement.innerHTML = page;
+    const loginForm = document.getElementById('login-form');
+    if (loginForm) {
+      loginForm.addEventListener('submit', logIn); //logIn()
+    }
+  }
+});
+
+async function logIn(event){
+  if (event) {
+    event.preventDefault();
+  }
+  const emailInput = document.querySelector('input[name="email"]');
+  const passwordInput = document.querySelector('input[name="password"]');
+
+  if (!emailInput || !passwordInput) {
+    return;
+  }
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try{
+  const response = await fetch("users.json");
+  const users = await response.json();
+  const user = users.students.find(user => user.email === email && user.password === password);
+  if (user) {
+    isLoggedIn = true;
+    document.getElementById('main').innerHTML = memberPage;
+  } else {
+    console.log("Invalid email or password");
+  }
+  console.log(users);
+  }catch(e){
+    console.log(e);
+  }
+  
+}
